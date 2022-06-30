60578de30a820200b27ac8792ac26d9ad9017a54
pavan02m <83412531+pavan02m@users.noreply.github.com>
2022-06-30 09:20:40 +0530

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUrl = "mongodb://localhost:27017/backendPrac";
-const connectTOMongo = () =>{
-    mongoose.connect(mongoUrl,()=>{
-        console.log("Connected to mongo successfuly");
-    })
-}
-module.exports = connectTOMongo;
